3775ad7a86a2e71f3a42ddc415c92aea7e00510e
Jianna Park <jiannajihyunpark@gmail.com>
2020-12-02 17:46:09 -0500

    feat: add form fields

--- Task_6/dbmodels.py
+++ Task_6/dbmodels.py
@@ -8,12 +8,20 @@ class Case(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     case_number = db.Column(db.Text())
+    open_date = db.Column(db.Date())
+    is_closed = db.Column(db.Boolean())
+    close_date = db.Column(db.Date())
+    country = db.Column(db.String())
     case_text = db.Column(db.Text())
     embeddings = db.Column(db.ARRAY(db.Float))
     embeddings_date = db.Column(db.Date())
 
-    def __init__(self, case_number, case_text, embeddings, embeddings_date):
+    def __init__(self, case_number, open_date, is_closed, close_date, country, case_text, embeddings, embeddings_date):
         self.case_number = case_number
+        self.open_date = open_date
+        self.is_closed = is_closed
+        self.close_date = close_date
+        self.country = country
         self.case_text = case_text
         self.embeddings = embeddings
         self.embeddings_date = embeddings.date
@@ -25,6 +33,10 @@ class Case(db.Model):
         return {
             'id': self.id,
             'case_number': self.case_number,
+            'open_date': self.open_date,
+            'is_closed': self.is_closed,
+            'close_date': self.close_date,
+            'country': self.country,
             'case_text': self.case_text,
             'embeddings': self.embeddings,
             'embeddings_date': self.embeddings_date
