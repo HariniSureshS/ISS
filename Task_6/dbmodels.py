acbc4f78352821a4bc8fc6d19e006b1089ccf804
Jianna Park <jiannajihyunpark@gmail.com>
2020-12-03 15:03:20 -0500

    add model fields

--- Task_6/dbmodels.py
+++ Task_6/dbmodels.py
@@ -4,15 +4,18 @@ class Case(db.Model):
     __tablename__ = 'cases'
 
     id = db.Column(db.Integer, primary_key=True)
-    case_number = db.Column(db.Text())
+    case_number = db.Column(db.String())
     open_date = db.Column(db.Date())
     is_closed = db.Column(db.Boolean())
     close_date = db.Column(db.Date())
     country = db.Column(db.String())
     service = db.Column(db.String())
     case_text = db.Column(db.Text())
-    embeddings = db.Column(db.ARRAY(db.Float))
-    embeddings_date = db.Column(db.Date())
+    embedding = db.Column(db.ARRAY(db.Float))
+    risk_score = db.Column(db.Float())
+    summary = db.Column(db.Text())
+    keywords = db.Column(db.ARRAY(db.Text()))
+    topic_word = db.Column(db.String())
 
     def __init__(
         self,
@@ -23,8 +26,11 @@ class Case(db.Model):
         country = None,
         service = None,
         case_text = None,
-        embeddings = None,
-        embeddings_date = None
+        embedding = None,
+        risk_score = None,
+        summary = None,
+        keywords = None,
+        topic_word = None
     ):
         self.case_number = case_number
         self.open_date = open_date
@@ -33,8 +39,11 @@ class Case(db.Model):
         self.country = country
         self.service = service
         self.case_text = case_text
-        self.embeddings = embeddings
-        self.embeddings_date = embeddings_date
+        self.embedding = embedding
+        self.risk_score = risk_score
+        self.summary = summary
+        self.keywords = keywords
+        self.topic_word = topic_word
 
     def __repr__(self):
         return "<Case {}>".format(self.case_number)
@@ -49,6 +58,9 @@ class Case(db.Model):
             'country': self.country,
             'service': self.service,
             'case_text': self.case_text,
-            'embeddings': self.embeddings,
-            'embeddings_date': self.embeddings_date
+            'embedding': self.embedding,
+            'risk_score': self.risk_score,
+            'summary': self.summary,
+            'keywords': self.keywords,
+            'topic_word': self.topic_word
         }
