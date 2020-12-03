6bc36138f52366f1248ffcc5083971c12852d001
Jianna Park <jiannajihyunpark@gmail.com>
2020-12-03 16:08:00 -0500

    chore: edit select choices

--- Task_6/form.py
+++ Task_6/form.py
@@ -9,7 +9,7 @@ class CaseForm(FlaskForm):
     is_closed = BooleanField('This is a closed case')
     close_date = DateField('Case Close Date (YYYY-MM-DD)', description='If case is closed', validators=[validators.Optional()])
     country = StringField('Country', description='Country where service is requested')
-    service = SelectField('Service Requested', choices=[(1, 'Child Protection'), (2, 'Children on the Move')], coerce=int, validate_choice=False)
+    service = SelectField('Service Requested', choices=[ 'Child Protection', 'Children on the Move'], validate_choice=False)
     case_text = TextAreaField('Case Text', description='Enter service(s) requested, background information and assessment (if any)')
     case_upload = FileField('File', description='Upload a case file or supplementary document', validators=[FileAllowed(['txt'],'txt file only')])
     submit = SubmitField('Submit')
