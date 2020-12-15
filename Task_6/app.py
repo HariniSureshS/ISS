ebabf3f4804f7f0803937c5f4e18c120a77c4eed
Jianna Park <jiannajihyunpark@gmail.com>
2020-12-15 16:55:39 -0500

    chore: remove babel

--- Task_6/app.py
+++ Task_6/app.py
@@ -1,16 +1,17 @@
 #!bin/python
 from flask import Flask, session, request, redirect, url_for, render_template
-from forms import CaseForm, QueryForm
+from flask_wtf.csrf import CSRFProtect
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_babel import Babel, _
 from sqlalchemy import or_, func
 from flask_migrate import Migrate
+from forms import CaseForm, QueryForm
+from text_converters import convert_pdf_to_txt
+from text_converters import doc_to_txt
+import os
 import json
 import datetime
 import pandas as pd
-import plotly
-import plotly.graph_objs as go
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
@@ -21,35 +22,23 @@ from models.keyword_extractor import KeywordExtractor
 from models.relation_extractor import get_entity_pairs
 from models.similar_cases import get_similar_cases
 from models.risk_factors import get_risk_factors
-from flask_wtf.csrf import CSRFProtect
-from text_converters import convert_pdf_to_txt
-from text_converters import doc_to_txt
-import os
-
 risk_model = load_model('models/risk_0.189.h5')
+from graphs.query_graphs import create_plot
+from graphs.relation_graph import generate_graph_from_relation
 
 app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY=b'\x94\xf4\xb6Eo\xc4?Kia\x852\xbc\xe9S~\xb7\xd0\xb7#a\x93g\xb8',
     WTF_CSRF_TIME_LIMIT=None,
     SQLALCHEMY_DATABASE_URI="postgresql://postgres:postgres@localhost:5432/iss",
-    SQLALCHEMY_ECHO= True)
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
+    SQLALCHEMY_ECHO=True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-babel = Babel(app)
-LANGUAGES = {
-    'en' : 'English',
-    'fr' : 'French',
-    'de' : 'German',
-    'zh' : 'Chinese',
-    'ko' : 'Korean',
-    'id' : 'Indonesian'
-}
 CSRFProtect(app)
 Bootstrap(app)
 
 import dbmodels
+
 
 @app.route('/')
 def main_menu():
@@ -60,7 +49,8 @@ def main_menu():
 # form page to input a new case and test out ML models
 @app.route('/models', methods=['GET', 'POST'])
 def enter_case():
-    session.pop('params', None)
+    session.clear()
+
     case_form = CaseForm()
     if request.method == 'POST' and case_form.validate_on_submit():
             session['form'] = request.form
@@ -79,42 +69,13 @@ def enter_case():
                 session['file'] = 'doc'
 
             elif not request.form.get('case_text', None):
-                return render_template('models.html', form=case_form)
+                return render_template('models.html', form = case_form)
             return redirect('/models/result')
-    return render_template('models.html', form=case_form)
-
-
-@app.route('/<page>/language/<language>')
-def set_language(page=None, language=None):
-    session['language'] = language
-    if page == 'models':
-        return redirect(url_for('enter_case'))
-    elif page == 'query':
-        return redirect(url_for('query_db'))
-
-
-@babel.localeselector
-def get_locale():
-    try:
-        language = session['language']
-    except KeyError:
-        language = None
-    if language is not None:
-        return language
-    return request.accept_languages.best_match(LANGUAGES.keys())
-
-
-@app.context_processor
-def inject_conf_var():
-    return dict(
-        AVAILABLE_LANGUAGES=LANGUAGES,
-        CURRENT_LANGUAGE=session.get('language', request.accept_languages.best_match(LANGUAGES.keys())))
+    return render_template('models.html', form = case_form)
 
 
 @app.route('/models/result')
 def show_result():
-    session.pop('params', None)
-
     form_input = None
     file_input = None
 
@@ -137,17 +98,19 @@ def show_result():
     else:
         case_text = form_input['case_text']
 
+    print('file input case text', case_text)
+
     headers = ['Case No.', 'Case Summary', 'Country', 'Status', 'Risk Score']
 
     return render_template('models_result.html',
-                           input=case_text,
-                           summary=summarize(case_text),
-                           keywords=get_keywords(case_text),
-                           relations=get_relations(case_text),
-                           risk_score=get_risk_score(case_text),
-                           abuse_types=get_abuse_types(case_text),
-                           similar_cases=get_similar(case_text),
-                           headers=headers)
+                           input = case_text,
+                           summary = summarize(case_text),
+                           keywords = get_keywords(case_text),
+                           relations = get_relations(case_text),
+                           risk_score = get_risk_score(case_text),
+                           abuse_types = get_abuse_types(case_text),
+                           similar_cases = get_similar(case_text),
+                           headers = headers)
 
 
 def summarize(case_text):
@@ -166,7 +129,7 @@ def get_relations(case_text):
     if not len(relations):
         return "No entity relations found"
     else:
-        return relations
+        return generate_graph_from_relation(relations)
 
 
 def get_risk_score(case_text):
@@ -205,7 +168,7 @@ def get_similar(case_text):
 # form page to query database
 @app.route('/query', methods=['GET', 'POST'])
 def query_db():
-    session.pop('params', None)
+    session.clear()
 
     query_form = QueryForm()
     if request.method == 'POST' and query_form.validate_on_submit():
@@ -323,93 +286,6 @@ def get_all_cases():
 
     except Exception as e:
         return 'Error: ' + str(e)
-
-
-def create_plot(df):
-    total_cases = df['country'].value_counts().sort_index().values.tolist()
-
-    countries = df['country'].tolist()
-    countries = list(set(countries))
-    countries.sort()
-
-    new_frame = pd.DataFrame(list(zip(countries,total_cases)),
-                     columns = ['Country','Totals'])
-
-    world = go.Figure(data = go.Choropleth(
-        locationmode = "country names",
-        locations = new_frame['Country'],
-        z = new_frame['Totals'],
-        text = new_frame['Country'],
-        colorscale ='Viridis',
-        autocolorscale = False,
-        reversescale = False,
-        marker_line_color = 'darkgray',
-        marker_line_width = 0.5,
-        colorbar_title = 'Reported Cases'
-    ))
-
-    world.update_layout(
-        title_text='Reported ISS Cases',
-        geo = dict(
-            showframe = False,
-            showcoastlines = False,
-            projection_type = 'equirectangular'
-        )
-    )
-
-    data_df = df.groupby('service').agg({'case_number':'count'})
-    labels = list(data_df.index.values)
-    values = data_df['case_number']
-    fig = go.Figure(data = [go.Pie(labels = labels, values = values, hole = .5)])
-
-    fig.update_layout(
-        title_text = 'Service Category'
-    )
-
-    df['month'] = pd.to_datetime(df["open_date"]).dt.strftime('%B')
-    df["Month"] = pd.to_datetime(df["open_date"]).dt.month
-    data_m = df.groupby(['Month','month']).agg({'case_number':'count'}).reset_index()
-    labels_m = list(data_m['month'])
-    values_m = data_m['case_number']
-
-    bar = [
-        go.Bar(
-            x = labels_m,
-            y = values_m,
-            marker = dict(color=['aliceblue','plum','mistyrose','thistle','khaki',
-                'beige','darksalmon','lightcyan','blanchedalmond','lightcoral',
-                'lavender','lightblue']),
-            text = values_m,
-            textposition = 'auto'
-        )
-    ]
-
-    fig1 = go.Figure(bar)
-    fig1.update_layout(
-        title = 'Monthly Cases',
-        xaxis_tickfont_size = 14,
-        yaxis = dict(
-            title = 'Frequency of Cases',
-            titlefont_size = 18,
-            tickfont_size = 16
-        ),
-        legend = dict(
-            x = 0,
-            y = 1.0,
-            bgcolor = 'rgba(255,255,255,0)',
-            bordercolor = 'rgba(255,255,255,0)'
-        ),
-        barmode = 'group',
-        xaxis_tickangle = -45,
-        bargap = 0.15,
-        bargroupgap = 0.1
-    )
-
-    worldJ = json.dumps(world, cls = plotly.utils.PlotlyJSONEncoder)
-    figS = json.dumps(fig, cls = plotly.utils.PlotlyJSONEncoder)
-    figM = json.dumps(fig1, cls = plotly.utils.PlotlyJSONEncoder)
-
-    return worldJ, figS, figM
 
 
 if __name__ == '__main__':
--- Task_6/forms.py
+++ Task_6/forms.py
@@ -2,34 +2,31 @@ from wtforms import SubmitField, StringField, RadioField, BooleanField, DateFiel
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 import datetime
-from flask_babel import lazy_gettext as _
+
 
 class CaseForm(FlaskForm):
-    case_text = TextAreaField(_('Case Text'), description=_('Enter service(s) requested, background information and assessment (if any)'))
+    case_text = TextAreaField('Case Text', description='Enter service(s) requested, background information and assessment (if any)')
 
-    case_upload = FileField(_('File'), description=_('Upload a case file or supplementary document'), validators=[FileAllowed(['txt','pdf','doc','docx'],_('txt, pdf, doc files only'))])
+    case_upload = FileField('File', description='Upload a case file or supplementary document', validators=[FileAllowed(['txt','pdf','doc','docx'], 'txt, pdf, doc files only')])
 
-    # from_language = SelectField(_('Case Text From Language'), description=_('Select the case text language'), choices=[('en','English'),('de','German')])
+    submit = SubmitField('Submit')
 
-    # to_language = SelectField(_('Case Text To Language'), description=_('Select the language to which the text needs to be converted'),choices=[('en','English'),('de','German')])
-
-    submit = SubmitField(_('Submit'))
 
 class QueryForm(FlaskForm):
-    get_open_close = RadioField(choices=[[0, _('Show open cases only')], [1, _('Show closed cases only')], [2, _('Show both open and closed cases')]], default=2)
+    get_open_close = RadioField(choices=[[0, 'Show open cases only'], [1, 'Show closed cases only'], [2, 'Show both open and closed cases']], default=2)
 
-    case_number = StringField(_('Show a case by case number'))
+    case_number = StringField('Show a case by case number')
 
-    open_date = DateField(_('Show cases that opened on or after this date'), description=_('Please follow this format: YYYY-MM-DD'), validators=[validators.Optional()])
+    open_date = DateField('Show cases that opened on or after this date', description='Please follow this format: YYYY-MM-DD', validators=[validators.Optional()])
 
-    close_date = DateField(_('Show cases that closed on or before this date, if closed'), description=_('Please follow this format: YYYY-MM-DD'), validators=[validators.Optional()])
+    close_date = DateField('Show cases that closed on or before this date, if closed', description='Please follow this format: YYYY-MM-DD', validators=[validators.Optional()])
 
-    country = StringField(_('Show cases where service is requested in this country'))
+    country = StringField('Show cases where service is requested in this country')
 
-    service = SelectField(_('Show cases where the following service is requested'), choices=['', _('Child Protection'), _('Children on the Move')], validate_choice=False)
+    service = SelectField('Show cases where the following service is requested', choices=['', 'Child Protection', 'Children on the Move'], validate_choice=False)
 
-    keywords = TextAreaField(_('Show cases that contain these words'), description=_('Please separate words with a comma'))
+    keywords = TextAreaField('Show cases that contain these words', description='Please separate words with a comma')
 
-    get_high_risk = BooleanField(_('Show high risk cases only'), description=_('High risk cases have risk score of or over 0.75'))
+    get_high_risk = BooleanField('Show high risk cases only', description='High risk cases have risk score of or over 0.75')
 
-    submit = SubmitField(_('Search'))
+    submit = SubmitField('Search')
